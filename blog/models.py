c201695f801dab7f41b18bbce144b201b7c13388
youssef-elouasbi-2 <youssef.elouasbi2022@gmail.com>
2022-12-22 15:26:19 +0100

    last

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 # Create your models here.
 
 class Profile(models.Model):
-    user = models.OneToOneRel(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birthdate = models.DateField(null=True, blank=True)
@@ -13,7 +15,7 @@ class Profile(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField(max_length=1500)
-    author = models.OneToOneRel(Profile, on_delete=models.CASCADE)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
     createddate = models.DateTimeField(auto_now_add=True)
     updateddate = models.DateTimeField(auto_now_add=True)
     def __str__(self):
